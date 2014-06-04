65edf88d821c02b3b95499ab118c6a2b37b63025
Pawel Rog <prog88@gmail.com>
2014-06-04 23:04:56 +0200

    RedisQParser with smembers

--- src/main/java/com/sematext/solr/redis/RedisQParser.java
+++ src/main/java/com/sematext/solr/redis/RedisQParser.java
@@ -1,6 +1,7 @@
 package com.sematext.solr.redis;
 
 import java.util.Collection;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.Query;
@@ -10,24 +11,30 @@ import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.search.QParser;
 import org.apache.solr.search.QueryParsing;
 import org.apache.solr.search.SyntaxError;
-import redis.clients.jedis.Jedis;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.JedisCommands;
 
 /**
  *
  */
 public class RedisQParser extends QParser {
+  static final Logger log = LoggerFactory.getLogger(RedisQParserPlugin.class);
+  
   protected Collection<String> terms;
 
-  private final Jedis jedisConnector;
+  private final JedisCommands redis;
 
-  RedisQParser (String qstr, SolrParams localParams, SolrParams params, SolrQueryRequest req, Jedis jedisConnector) {
+  RedisQParser (String qstr, SolrParams localParams, SolrParams params, SolrQueryRequest req,
+          JedisCommands redis) {
     super(qstr, localParams, params, req);
+    this.redis = redis;
 
     String redisMethod = localParams.get("method");
     String redisKey = localParams.get("key");
-    this.jedisConnector = jedisConnector;
+
     if (redisMethod.compareToIgnoreCase("smembers") == 0) {
-      
+      terms = redis.smembers(redisKey);
     }
 
     Collection<String> terms = null;
@@ -37,6 +44,7 @@ public class RedisQParser extends QParser {
   public Query parse() throws SyntaxError {
     String fieldName = localParams.get(QueryParsing.V, null);
     BooleanQuery booleanQuery = new BooleanQuery(true);
+    log.debug();
     for (String term : terms) {
       TermQuery termQuery = new TermQuery(new Term(qstr, fieldName));
       booleanQuery.add(termQuery, BooleanClause.Occur.SHOULD);
--- src/main/java/com/sematext/solr/redis/RedisQParserPlugin.java
+++ src/main/java/com/sematext/solr/redis/RedisQParserPlugin.java
@@ -1,42 +1,64 @@
 package com.sematext.solr.redis;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.common.util.NamedList;
 import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.search.QParser;
 import org.apache.solr.search.QParserPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCommands;
+import redis.clients.jedis.JedisPool;
 
 /**
  * Create a boolean query with multiple tokens.
  */
 public class RedisQParserPlugin extends QParserPlugin {
   public static final String NAME = "redis";
-  public static final String HOST_FIELD = "hosts";
+  public static final String HOST_FIELD = "host";
+  
+  static final Logger log = LoggerFactory.getLogger(RedisQParserPlugin.class);
 
-  public final List<String> hosts = Arrays.asList("localhost");
-
-  public Jedis jedisConnector;
+  private Set<HostAndPort> hosts = new HashSet<>();
+  
+  private JedisPool jedisConnectorPool;
+  private JedisCommands jedis;
 
 
   @Override
   public QParser createParser(String qstr, SolrParams localParams, SolrParams params, SolrQueryRequest req) {
-    String redisMethod = localParams.get("method");
-    String redisKey = localParams.get("key");
-    return new RedisQParser(qstr, localParams, params, req, jedisConnector);
-  }
-
-  @Override
-  public void init(NamedList args) {
-    if (args != null) {
-      Object hosts = args.get(HOST_FIELD);
-      if (hosts instanceof NamedList) {
-        SolrParams hostsConfiguration = SolrParams.toSolrParams((NamedList) hosts);
+    Jedis jedisConnector = null;
+    try {
+      jedisConnector = jedisConnectorPool.getResource();
+      return new RedisQParser(qstr, localParams, params, req, jedisConnector);
+    } finally {
+      if (jedisConnector != null) {
+        jedisConnectorPool.returnResource(jedisConnector);
       }
     }
   }
-
-
+  
+  @Override
+  public void init(NamedList args) {
+    if (args != null) {
+      SolrParams queryParserConfigurationParameters = SolrParams.toSolrParams(args);
+      Object hostConfiguration = args.get(HOST_FIELD);
+      if (hostConfiguration instanceof String) {
+        String host = (String) hostConfiguration;
+        log.info("Initializing RedisQParserPlugin with host: " + host);
+        String[] hostAndPort = host.split(":");
+        if (hostAndPort.length == 2) {
+          jedisConnectorPool = new JedisPool(hostAndPort[0], Integer.parseInt(hostAndPort[1]));
+        } else {
+          jedisConnectorPool = new JedisPool(host);
+        }
+      } else {
+        log.info("Initialization of RedisQParserPlugin failed. No redis host configuration");
+      }
+    }
+  }
 }
