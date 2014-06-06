8583bc25e38e770b2ac706740b976cbede712d55
Pawel Rog <prog88@gmail.com>
2014-06-06 16:43:25 +0200

    Unit tests add StandardAnalyzer instead custom Analyzer Stub

--- src/test/java/com/sematext/solr/redis/TestRedisQParser.java
+++ src/test/java/com/sematext/solr/redis/TestRedisQParser.java
@@ -1,18 +1,13 @@
 package com.sematext.solr.redis;
 
 import java.io.IOException;
-import java.io.Reader;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Random;
 import java.util.Set;
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.MockAnalyzer;
-import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.core.SimpleAnalyzer;
-import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.util.Version;
 import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.schema.IndexSchema;
@@ -80,69 +75,12 @@ public class TestRedisQParser {
     when(localParamsMock.get(QueryParsing.V)).thenReturn("string_field");
     when(jedisMock.smembers(any(String.class))).thenReturn(new HashSet<String>(Arrays.asList("123", "321")));
     when(requestMock.getSchema()).thenReturn(schema);
-    //when(schema.getQueryAnalyzer()).thenReturn(new SimpleAnalyzer());
+    when(schema.getQueryAnalyzer()).thenReturn(new StandardAnalyzer(Version.LUCENE_48));
     redisQParser = new RedisQParser("string_field", localParamsMock, paramsMock, requestMock, jedisMock);
     verify(jedisMock).smembers("simpleKey");
     Query query = redisQParser.parse();
     Set<Term> terms = new HashSet<Term>();
     query.extractTerms(terms);
     Assert.assertEquals(2, terms.size());
-  }
-
-  private final class TokenStreamStub extends TokenStream {
-
-    private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
-
-    private int index;
-    private final String tokens[];
-
-    public TokenStreamStub(String ... tokens) {
-      this.tokens = tokens;
-      reset();
-    }
-    
-    @Override
-    public void reset() {
-      index = 0;
-    }
-
-    @Override
-    public boolean incrementToken() throws IOException {
-      clearAttributes();
-      if (index == tokens.length) {
-        return false;
-      } else {
-        String token = tokens[index++];
-        termAtt.setEmpty();
-        termAtt.resizeBuffer(token.length());
-        termAtt.append(token);
-        return true;
-      }
-    }
-  }
-
-  private final class AnalyzerStub extends Analyzer {
-
-    private TokenStream tokenStream;
-
-    public AnalyzerStub(TokenStream tokenStream) {
-      super(new ReuseStrategy() {
-
-        @Override
-        public TokenStreamComponents getReusableComponents(Analyzer analyzer, String fieldName) {
-          return null;
-        }
-
-        @Override
-        public void setReusableComponents(Analyzer analyzer, String fieldName, TokenStreamComponents components) {
-        }
-      });
-      this.tokenStream = tokenStream;
-    }
-    
-    @Override
-    protected TokenStreamComponents createComponents(String fieldName, Reader reader) {
-      return new TokenStreamComponents(null, tokenStream);
-    }
   }
 }
