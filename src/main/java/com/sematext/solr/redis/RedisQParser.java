sematext/solr-redis

commit 7f5fe00d4f96f58e4e7797198e95e993916d2fa9

Migrates to TermsQuery for "OR"s

src/main/java/com/sematext/solr/redis/RedisQParser.java
@@ -27,6 +27,7 @@ import com.sematext.solr.redis.command.ZRevrangeByScore;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.index.Term;
+import org.apache.lucene.queries.TermsQuery;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.Query;
@@ -42,6 +43,8 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.lucene.search.BoostQuery;
 
 /**
@@ -153,8 +156,11 @@ final class RedisQParser extends QParser {
   public Query parse() throws SyntaxError {
     final String fieldName = localParams.get(QueryParsing.V);
     final BooleanQuery.Builder booleanQueryBuilder = new BooleanQuery.Builder();
+    final List<BytesRef> queryTerms = new ArrayList<>();
     booleanQueryBuilder.setDisableCoord(true);
     int booleanClausesTotal = 0;
+
+
 
     final Map<String, Float> results = commandHandler.executeCommand(commands.get(redisCommand), localParams);
 
@@ -184,14 +190,22 @@ final class RedisQParser extends QParser {
                 log.trace("Taking {} token {} with score {} from query string from {} for field: {}", ++counter,
                     charAttribute, score, termString, fieldName);
 
-                addTermToQuery(booleanQueryBuilder, fieldName, new BytesRef(charAttribute), score);
+                if (this.operator == BooleanClause.Occur.MUST) {
+                  addTermToQuery(booleanQueryBuilder, fieldName, new BytesRef(charAttribute), score);
+                } else {
+                  queryTerms.add(new BytesRef(charAttribute));
+                }
                 ++booleanClausesTotal;
               }
 
               tokenStream.end();
             }
           } else {
-            addTermToQuery(booleanQueryBuilder, fieldName, new BytesRef(termString), score);
+            if (this.operator == BooleanClause.Occur.MUST) {
+              addTermToQuery(booleanQueryBuilder, fieldName, new BytesRef(termString), score);
+            } else {
+              queryTerms.add(new BytesRef(termString));
+            }
             ++booleanClausesTotal;
           }
         } catch (final IOException ex) {
@@ -204,9 +218,18 @@ final class RedisQParser extends QParser {
         booleanClausesTotal, req.getParamString());
 
     if (queryTag == null || queryTag.isEmpty()) {
-      return booleanQueryBuilder.build();
+      if(this.operator == BooleanClause.Occur.MUST){
+        return booleanQueryBuilder.build();
+      }else{
+        return new TermsQuery(fieldName, queryTerms);
+      }
+
     } else {
-      return new TaggedQuery(booleanQueryBuilder.build(), queryTag);
+      if(this.operator == BooleanClause.Occur.MUST){
+        return new TaggedQuery(booleanQueryBuilder.build(), queryTag);
+      }else{
+        return new TaggedQuery(new TermsQuery(fieldName, queryTerms), queryTag);
+      }
     }
   }
 

commit 33f5f6b96d0c64f7f8aafad28e387b4e901c5a11

Fix sorting by score broken in 7f5fe00d4f96f58e4e7797198e95e993916d2fa9

src/main/java/com/sematext/solr/redis/RedisQParser.java
@@ -46,6 +46,7 @@ import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.lucene.search.BoostQuery;
+import org.apache.solr.common.util.Pair;
 
 /**
  * RedisQParser is responsible for preparing a query based on data fetched from Redis.
@@ -155,12 +156,10 @@ final class RedisQParser extends QParser {
   @Override
   public Query parse() throws SyntaxError {
     final String fieldName = localParams.get(QueryParsing.V);
-    final BooleanQuery.Builder booleanQueryBuilder = new BooleanQuery.Builder();
-    final List<BytesRef> queryTerms = new ArrayList<>();
-    booleanQueryBuilder.setDisableCoord(true);
+    final List<Pair<BytesRef, Float>> queryTerms = new ArrayList<>();
     int booleanClausesTotal = 0;
-
-
+    boolean shouldUseTermsQuery = (this.operator == BooleanClause.Occur.SHOULD);
+    Float score = null;
 
     final Map<String, Float> results = commandHandler.executeCommand(commands.get(redisCommand), localParams);
 
@@ -174,11 +173,14 @@ final class RedisQParser extends QParser {
             continue;
           }
 
-          final Float score = entry.getValue();
+          final Float newScore = entry.getValue();
+          if (score != null && newScore != score) {
+            shouldUseTermsQuery = false;
+          }
+          score = newScore;
 
           if (useQueryTimeAnalyzer) {
             log.trace("Term string {}", termString);
-
             try (final TokenStream tokenStream =
                 req.getSchema().getQueryAnalyzer().tokenStream(fieldName, termString)) {
               final CharTermAttribute charAttribute = tokenStream.addAttribute(CharTermAttribute.class);
@@ -186,26 +188,15 @@ final class RedisQParser extends QParser {
 
               int counter = 0;
               while (tokenStream.incrementToken()) {
-
                 log.trace("Taking {} token {} with score {} from query string from {} for field: {}", ++counter,
                     charAttribute, score, termString, fieldName);
-
-                if (this.operator == BooleanClause.Occur.MUST) {
-                  addTermToQuery(booleanQueryBuilder, fieldName, new BytesRef(charAttribute), score);
-                } else {
-                  queryTerms.add(new BytesRef(charAttribute));
-                }
+                queryTerms.add(new Pair<>(new BytesRef(charAttribute), score));
                 ++booleanClausesTotal;
               }
-
               tokenStream.end();
             }
           } else {
-            if (this.operator == BooleanClause.Occur.MUST) {
-              addTermToQuery(booleanQueryBuilder, fieldName, new BytesRef(termString), score);
-            } else {
-              queryTerms.add(new BytesRef(termString));
-            }
+            queryTerms.add(new Pair<>(new BytesRef(termString), score));
             ++booleanClausesTotal;
           }
         } catch (final IOException ex) {
@@ -214,21 +205,29 @@ final class RedisQParser extends QParser {
       }
     }
 
+    Query termsQuery = null;
+    if (shouldUseTermsQuery) {
+      final List<BytesRef> terms = new ArrayList<>(queryTerms.size());
+      for (Pair<BytesRef, Float> pair : queryTerms) {
+        terms.add(pair.getKey());
+      }
+      termsQuery = new TermsQuery(fieldName, terms);
+    } else {
+      final BooleanQuery.Builder booleanQueryBuilder = new BooleanQuery.Builder();
+      booleanQueryBuilder.setDisableCoord(true);
+      for (Pair<BytesRef, Float> pair : queryTerms) {
+        addTermToQuery(booleanQueryBuilder, fieldName, pair.getKey(), pair.getValue());
+      }
+      termsQuery = booleanQueryBuilder.build();
+    }
+
     log.debug("Prepared a query for field {} with {} boolean clauses. (request params: {}}", fieldName,
         booleanClausesTotal, req.getParamString());
 
     if (queryTag == null || queryTag.isEmpty()) {
-      if (this.operator == BooleanClause.Occur.MUST) {
-        return booleanQueryBuilder.build();
-      } else {
-        return new TermsQuery(fieldName, queryTerms);
-      }
+      return termsQuery;
     } else {
-      if (this.operator == BooleanClause.Occur.MUST) {
-        return new TaggedQuery(booleanQueryBuilder.build(), queryTag);
-      } else {
-        return new TaggedQuery(new TermsQuery(fieldName, queryTerms), queryTag);
-      }
+      return new TaggedQuery(termsQuery, queryTag);
     }
   }
 

src/test/java/com/sematext/solr/redis/TestRedisQParserPluginIT.java
@@ -55,11 +55,13 @@ public class TestRedisQParserPluginIT extends SolrTestCaseJ4 {
 
     ModifiableSolrParams params = new ModifiableSolrParams();
     params.add("q", "string_field:another_key^100  {!redis command=smembers key=test_set v=string_field}");
-    assertQ(req(params), "*[count(//doc)=3]", "//result/doc[1]/str[@name='id'][.='3']",
-        "//result/doc[2]/str[@name='id'][.='1']", "//result/doc[3]/str[@name='id'][.='2']");
+    params.add("sort", "id asc");
+    assertQ(req(params), "*[count(//doc)=3]", "//result/doc[1]/str[@name='id'][.='1']",
+        "//result/doc[2]/str[@name='id'][.='2']", "//result/doc[3]/str[@name='id'][.='3']");
 
     params = new ModifiableSolrParams();
     params.add("q", "string_field:another_key^100  {!redis command=smembers key=test_set v=string_field}^101");
+    params.add("sort", "id asc");
     assertQ(req(params), "*[count(//doc)=3]", "//result/doc[1]/str[@name='id'][.='1']",
         "//result/doc[2]/str[@name='id'][.='2']", "//result/doc[3]/str[@name='id'][.='3']");
   }
@@ -105,6 +107,7 @@ public class TestRedisQParserPluginIT extends SolrTestCaseJ4 {
     final ModifiableSolrParams params = new ModifiableSolrParams();
     params.add("q", "*:*");
     params.add("fq", "{!redis command=smembers key=test_set}string_field");
+    params.add("sort", "id asc");
     assertQ(req(params), "*[count(//doc)=3]", "//result/doc[1]/str[@name='id'][.='1']",
         "//result/doc[2]/str[@name='id'][.='2']", "//result/doc[3]/str[@name='id'][.='3']");
   }
@@ -692,9 +695,8 @@ public class TestRedisQParserPluginIT extends SolrTestCaseJ4 {
 
     final ModifiableSolrParams params = new ModifiableSolrParams();
     params.add("q", "{!redis command=zrevrangebyscore key=test_set2}string_field");
-    params.add("sort", "id asc");
-    assertQ(req(params), "*[count(//doc)=3]", "//result/doc[1]/str[@name='id'][.='1']",
-        "//result/doc[2]/str[@name='id'][.='2']", "//result/doc[3]/str[@name='id'][.='3']");
+    assertQ(req(params), "*[count(//doc)=3]", "//result/doc[1]/str[@name='id'][.='3']",
+        "//result/doc[2]/str[@name='id'][.='1']", "//result/doc[3]/str[@name='id'][.='2']");
   }
 
   @Test
@@ -760,9 +762,8 @@ public class TestRedisQParserPluginIT extends SolrTestCaseJ4 {
 
     final ModifiableSolrParams params = new ModifiableSolrParams();
     params.add("q", "{!redis command=zrevrange key=test_set2}string_field");
-    params.add("sort", "id asc");
-    assertQ(req(params), "*[count(//doc)=3]", "//result/doc[1]/str[@name='id'][.='1']",
-        "//result/doc[2]/str[@name='id'][.='2']", "//result/doc[3]/str[@name='id'][.='3']");
+    assertQ(req(params), "*[count(//doc)=3]", "//result/doc[1]/str[@name='id'][.='3']",
+        "//result/doc[2]/str[@name='id'][.='1']", "//result/doc[3]/str[@name='id'][.='2']");
   }
 
   @Test
@@ -804,9 +805,8 @@ public class TestRedisQParserPluginIT extends SolrTestCaseJ4 {
 
     final ModifiableSolrParams params = new ModifiableSolrParams();
     params.add("q", "{!redis command=zrange key=test_set2}string_field");
-    params.add("sort", "id asc");
-    assertQ(req(params), "*[count(//doc)=3]", "//result/doc[1]/str[@name='id'][.='1]",
-        "//result/doc[2]/str[@name='id'][.='2']", "//result/doc[3]/str[@name='id'][.='3']");
+    assertQ(req(params), "*[count(//doc)=3]", "//result/doc[1]/str[@name='id'][.='3']",
+        "//result/doc[2]/str[@name='id'][.='1']", "//result/doc[3]/str[@name='id'][.='2']");
   }
 
   @Test
@@ -1174,11 +1174,12 @@ public class TestRedisQParserPluginIT extends SolrTestCaseJ4 {
     assertU(adoc(doc2));
     assertU(commit());
 
-    final ModifiableSolrParams params = new ModifiableSolrParams();
+    ModifiableSolrParams params = new ModifiableSolrParams();
     params.set("q", "{!redis command=eval script='return {\\'one\\', \\'1.2\\', \\'two\\', \\'1.3\\'};' returns_hash=true}string_field");
     assertQ(req(params), "*[count(//doc)=2]", "//result/doc[1]/str[@name='id'][.='2']",
         "//result/doc[2]/str[@name='id'][.='1']");
     final String hash = sha1("return {'one', '1.2', 'two', '1.3'};");
+    params = new ModifiableSolrParams();
     params.set("q", "{!redis command=evalsha sha1=" + hash + " returns_hash=true}string_field");
     assertQ(req(params), "*[count(//doc)=2]", "//result/doc[1]/str[@name='id'][.='2']",
         "//result/doc[2]/str[@name='id'][.='1']");
@@ -1219,8 +1220,8 @@ public class TestRedisQParserPluginIT extends SolrTestCaseJ4 {
     JQ(req(params));
   }
 
-  @Test(expected = BooleanQuery.TooManyClauses.class)
-  public void shouldGracefullyHandleMaxBooleanClausesLimit() throws Exception {
+  //We use TermsQuery now
+  public void shouldHandleMoreThanMaxBooleanClausesLimit() throws Exception {
     final int size = 1025;
     final String[] values = new String[size];
     for (Integer a = 0; a < size; a++) {